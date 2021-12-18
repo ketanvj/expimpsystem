fc04c6bc9251ab04971e6767fa65e71b8097b920
Ketan J <ketanvj@yahoo.com>
2021-12-18 16:14:59 +0530

    Update MyFirstTestNGTest.java

--- MyFirstTestNGTest.java
+++ MyFirstTestNGTest.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-
+//commments.
 public class MyFirstTestNGTest {
 	WebDriver driver; //Browser
 
